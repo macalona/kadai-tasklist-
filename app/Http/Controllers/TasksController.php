f3d2f13d1c94e13ce112aee73222ed06d72ab101
macalona <ankozimaaa@yahoo.co.jp>
2019-07-22 06:41:37 +0000

    koredeowarida!

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -42,12 +42,15 @@ class TasksController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+     
     public function store(Request $request)
     {   
+        $this->validate($request, [
+            'status' => 'required|max:10',
+        ]);
       
-        
         $task = new Task;
-        $task->status = $task->status; 
+        $task->status = $request->status; 
         $task->content = $request->content;
         $task->save();
 
@@ -93,7 +96,9 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {   
-       
+         $this->validate($request, [
+            'status' => 'required|max:10',
+        ]);
         
         $task = Task::find($id);
          $task->status = $request->status;
