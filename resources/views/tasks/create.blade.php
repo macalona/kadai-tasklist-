a38d77e8bb81e315442f04468d1a3b36807bb7af
macalona <ankozimaaa@yahoo.co.jp>
2019-07-22 09:30:03 +0000

    asdgasdg

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <!-- ここにページ毎のコンテンツを書く -->
- <h1>メッセージ新規作成ページ</h1>
+ <h1>タスク新規作成ページ</h1>
 
     <div class="row">
         <div class="col-6">
@@ -15,7 +15,7 @@
                 </div>
                 
                 <div class="form-group">
-                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
         
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <!-- ここにページ毎のコンテンツを書く -->
-   <h1>メッセージ一覧</h1>
+   <h1>タスク一覧</h1>
 
     @if (count($tasks) > 0)
         <table class="table table-striped">
@@ -11,7 +11,7 @@
                 <tr>
                     <th>id</th>
                     <th>ステータス</th>
-                    <th>メッセージ</th>
+                    <th>タスク</th>
                 </tr>
             </thead>
             <tbody>
@@ -25,5 +25,7 @@
             </tbody>
         </table>
     @endif
+    
+    {!! link_to_route('tasks.create', '新規タスクの投稿', null, ['class' => 'btn btn-primary']) !!}
 
 @endsection
